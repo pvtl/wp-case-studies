766203e42cf6cda2369ae0c2d5e033a56a41d593
Matt Mcnamee <matt@mcnamee.co>
2018-05-08 09:44:10 +1000

    Convert to more generic class

--- case-studies.php
+++ case-studies.php
@@ -1,4 +1,5 @@
 <?php
+// phpcs:disable PSR1.Files.SideEffects
 /**
  * Plugin Name:     Case Studies by Pivotal
  * Plugin URI:      https://github.com/pvtl/wp-case-studies.git
@@ -12,51 +13,18 @@
  * @package         Case_Studies
  */
 
-/**
- * Setup the custom post type, tax and add fields
- */
-include(dirname(__FILE__). '/post-types/case-study.php');
-include(dirname(__FILE__). '/taxonomies/case-study-category.php');
-include(dirname(__FILE__). '/custom-fields/case-study.php');
+namespace App\Plugins;
 
-/**
- * Show admin error if ACF is not installed
- */
-if (!function_exists('the_field')) {
-    if (!function_exists('acf_not_installed_admin_notice__error')) {
-        function acf_not_installed_admin_notice__error()
-        {
-            $class = 'notice notice-error';
-            $message = __('Please install Advanced Custom Fields Pro for your plugins to work correctly.', 'case-studies-text-domain');
-            printf('<div class="%1$s"><p>%2$s</p></div>', esc_attr($class), esc_html($message));
-        }
-
-        add_action('admin_notices', 'acf_not_installed_admin_notice__error');
-    }
-}
-
-/**
- * Front-end templates
- */
-function case_studies_templates($template)
+class PvtlCaseStudies
 {
-    if (is_archive('case-study') && $template !== locate_template(array("archive-case-study.php"))) {
-        return plugin_dir_path(__FILE__) .'resources/views/archive.php';
-    }
-    if (is_singular('case-study') && $template !== locate_template(array("single-case-study.php"))) {
-        return plugin_dir_path(__FILE__) .'resources/views/single.php';
-    }
+    // The name of the plugin (for cosmetic purposes)
+    protected $pluginName = 'Case Studies';
 
-    return $template;
-}
+    // The post type's slug eg. case-study
+    protected $postType = 'case-study';
 
-add_filter('template_include', 'case_studies_templates');
-
-/**
- * On Install - copy over the assets
- */
-function case_studies_activate() {
-    $to_copy = array(
+    // The assets (css/js) to be copied from the plugin to the theme
+    protected $frontendAssets = array(
         array(
             'from' => 'js/',
             'to' => 'js/plugins/',
@@ -68,32 +36,99 @@ function case_studies_activate() {
             'name' => '_case-studies.scss',
         ),
     );
-    $theme_asset_dir = get_template_directory() . '/src/assets/';
-    $plugin_asset_dir = plugin_dir_path(__FILE__) . 'resources/assets/';
-    // Only continue if the PVTL theme directory exists
-    if (file_exists($theme_asset_dir)) {
-        // For each file to copy across
-        foreach ($to_copy as $file) {
-            // Ignore if the file already exists
-            if (file_exists($theme_asset_dir . $file['to'] . $file['name'])) {
-                continue;
-            }
 
-            // Ensure the directory exists
-            mkdir($theme_asset_dir . $file['to'], 0755, true);
+    public function __construct()
+    {
+        // Setup the custom post type, tax and add fields
+        include(dirname(__FILE__). '/post-types/' . $this->postType . '.php');
+        include(dirname(__FILE__). '/taxonomies/' . $this->postType . '-category.php');
+        include(dirname(__FILE__). '/custom-fields/' . $this->postType . '.php');
 
-            // Copy that file
-            if(
-                !@copy(
-                    $plugin_asset_dir . $file['from'] . $file['name'],
-                    $theme_asset_dir . $file['to'] . $file['name']
-                )
-            ) {
-                print_r(error_get_last());
-                die();
+        // Call the actions/hooks
+        add_action('admin_notices', array($this, 'acfNotInstalledAdminError'));
+        add_filter('template_include', array($this, 'fronendTemplates'));
+        register_activation_hook(__FILE__, array($this, 'onActivate'));
+    }
+
+    /**
+     * Display Error in Admin when is not installed
+     *
+     * @return void
+     */
+    public function acfNotInstalledAdminError()
+    {
+        if (!function_exists('the_field')) {
+            $class = 'notice notice-error';
+            $message = 'Please install <a target="_blank"
+                href="https://github.com/wp-premium/advanced-custom-fields-pro">
+                Advanced Custom Fields Pro</a> for the ' . $this->pluginName . ' plugin to
+                work correctly.';
+
+            printf('<div class="%1$s"><p>%2$s</p></div>', esc_attr($class), $message);
+        }
+    }
+
+    /**
+     * Display front-end templates
+     *
+     * @param str $template
+     * @return void
+     */
+    public function fronendTemplates($template)
+    {
+        // When an archive - try the theme version otherwise use the default plugin version
+        if (is_archive($this->postType)
+            && $template !== locate_template(array('archive-' . $this->postType . '.php'))) {
+            return plugin_dir_path(__FILE__) .'resources/views/archive.php';
+        }
+
+        // When a single - try the theme version otherwise use the default plugin version
+        if (is_singular($this->postType)
+            && $template !== locate_template(array('single-' . $this->postType . '.php'))) {
+            return plugin_dir_path(__FILE__) .'resources/views/single.php';
+        }
+
+        return $template;
+    }
+
+    /**
+     * On Plugin Activate, copy over front-end assets
+     *
+     * @return void
+     */
+    public function onActivate()
+    {
+        $themeAssetDir = get_template_directory() . '/src/assets/';
+        $pluginAssetDir = plugin_dir_path(__FILE__) . 'resources/assets/';
+
+        // Only continue if the PVTL theme directory exists
+        if (file_exists($themeAssetDir)) {
+            // For each file to copy across
+            foreach ((array)$this->frontendAssets as $file) {
+                // Ignore if the file already exists
+                if (file_exists($themeAssetDir . $file['to'] . $file['name'])) {
+                    continue;
+                }
+
+                // Ensure the directory exists
+                mkdir($themeAssetDir . $file['to'], 0755, true);
+
+                // Copy that file
+                if (!@copy(
+                    $pluginAssetDir . $file['from'] . $file['name'],
+                    $themeAssetDir . $file['to'] . $file['name']
+                )) {
+                    // Print any errors to the admin panel
+                    print_r(error_get_last());
+                    die();
+                }
             }
         }
     }
 }
 
-register_activation_hook(__FILE__, 'case_studies_activate');
+if (!defined('ABSPATH')) {
+    exit;  // Exit if accessed directly
+}
+
+$pvtlCaseStudies = new PvtlCaseStudies();
